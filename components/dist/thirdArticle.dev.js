868a66a2693d1c26e72e2f2c0aff09935cece530
a.bubelev <bubeleu@gmail.com>
2022-09-19 23:17:34 +0300

    bug

--- components/dist/thirdArticle.dev.js
+++ components/dist/thirdArticle.dev.js
@@ -1,14 +1,17 @@
 "use strict";
 
 // const call = document.querySelector('.nameArticle');
-// const btnLeft = document.querySelector('.btn-left');
-// const btnRight = document.querySelector('.btn-right');
-// const carusel = document.querySelector('.row-carusel');
+// const btnLeft = document.querySelector('.bntLeft');
+var btnRight = document.querySelector('.btnRight'); // const carusel = document.querySelector('.row-carusel');
 // const imgCenter = document.querySelector('.img-center')
 // const width = -43;
 // btnLeft.onclick = function(){
-//     carusel.style.transform = `translateX(${10 - width}%)`;
+//     carusel.style.transform = `translateX(${5 - width}%)`;
 // }
+// btnRight.onclick = function(){
+//     carusel.style.transform = `translateX(${5 + width}%)`;
+// }
+
 var imgAll = document.querySelectorAll('.item-img');
 console.log(imgAll);
 var slider = [];
@@ -20,3 +23,21 @@ for (var i = 0; i < imgAll.length; i++) {
 console.log(slider);
 var step = 0;
 var offset = 0;
+
+function draw() {
+  var img = document.createElement('img');
+  img.src = slider[step];
+  img.classList.add('.item-img');
+  img.style.transform = 'translateX(-83%)';
+  document.querySelector('.row-carusel').appendChild(img);
+
+  if (step + 1 == slider.length) {
+    step = 0;
+  } else {
+    step++;
+  }
+}
+
+btnRight.onclick = function () {
+  draw();
+};
--- components/thirdArticle.js
+++ components/thirdArticle.js
@@ -1,21 +1,41 @@
 // const call = document.querySelector('.nameArticle');
-// const btnLeft = document.querySelector('.btn-left');
-// const btnRight = document.querySelector('.btn-right');
+// const btnLeft = document.querySelector('.bntLeft');
+const btnRight = document.querySelector('.btnRight');
 // const carusel = document.querySelector('.row-carusel');
 // const imgCenter = document.querySelector('.img-center')
 // const width = -43;
 
 // btnLeft.onclick = function(){
-//     carusel.style.transform = `translateX(${10 - width}%)`;
+//     carusel.style.transform = `translateX(${5 - width}%)`;
+// }
+// btnRight.onclick = function(){
+//     carusel.style.transform = `translateX(${5 + width}%)`;
 // }
 const imgAll = document.querySelectorAll('.item-img');
 console.log(imgAll)
 let slider = [];
-for(let i = 0; i<imgAll.length; i++){
+for (let i = 0; i < imgAll.length; i++) {
     slider[i] = imgAll[i].src;
-    
+
 }
 console.log(slider)
 
 let step = 0;
 let offset = 0;
+
+function draw() {
+    let img = document.createElement('img');
+    img.src = slider[step];
+    img.classList.add('.item-img');
+    img.style.transform = 'translateX(-83%)';
+    document.querySelector('.row-carusel').appendChild(img);
+    if (step + 1 == slider.length) {
+        step = 0
+    } else {
+        step++;
+    }
+
+}
+btnRight.onclick = function () {
+    draw()
+}
