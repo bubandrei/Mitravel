142dbefdf83403969bff65a8543f05b78848bd65
a.bubelev <bubeleu@gmail.com>
2022-09-19 22:55:15 +0300

    carusel

--- components/dist/thirdArticle.dev.js
+++ components/dist/thirdArticle.dev.js
@@ -1,6 +1,22 @@
 "use strict";
 
-var call = document.querySelector('.nameArticle');
-var btnLeft = document.querySelector('.btn-left');
-var btnRight = document.querySelector('.btn-right');
-call.style.color = 'red';
+// const call = document.querySelector('.nameArticle');
+// const btnLeft = document.querySelector('.btn-left');
+// const btnRight = document.querySelector('.btn-right');
+// const carusel = document.querySelector('.row-carusel');
+// const imgCenter = document.querySelector('.img-center')
+// const width = -43;
+// btnLeft.onclick = function(){
+//     carusel.style.transform = `translateX(${10 - width}%)`;
+// }
+var imgAll = document.querySelectorAll('.item-img');
+console.log(imgAll);
+var slider = [];
+
+for (var i = 0; i < imgAll.length; i++) {
+  slider[i] = imgAll[i].src;
+}
+
+console.log(slider);
+var step = 0;
+var offset = 0;
--- components/thirdArticle.js
+++ components/thirdArticle.js
@@ -1,4 +1,21 @@
-const call = document.querySelector('.nameArticle');
-const btnLeft = document.querySelector('.btn-left');
-const btnRight = document.querySelector('.btn-right')
-call.style.color = 'red';
+// const call = document.querySelector('.nameArticle');
+// const btnLeft = document.querySelector('.btn-left');
+// const btnRight = document.querySelector('.btn-right');
+// const carusel = document.querySelector('.row-carusel');
+// const imgCenter = document.querySelector('.img-center')
+// const width = -43;
+
+// btnLeft.onclick = function(){
+//     carusel.style.transform = `translateX(${10 - width}%)`;
+// }
+const imgAll = document.querySelectorAll('.item-img');
+console.log(imgAll)
+let slider = [];
+for(let i = 0; i<imgAll.length; i++){
+    slider[i] = imgAll[i].src;
+    
+}
+console.log(slider)
+
+let step = 0;
+let offset = 0;
